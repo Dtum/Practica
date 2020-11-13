6a70592aaac930bb99deb38b7dac2cfbf4f3d106
Dtum <71152051+Dtum@users.noreply.github.com>
2020-11-13 03:09:21 -0600

    Add files via upload

---
+++ Practica_8.py
@@ -0,0 +1,43 @@
+import os
+from ftplib import FTP
+import ftplib
+
+#Nos Conectamos Al Servidor
+ftp = FTP("ftp.us.debian.org")
+if ftp.login():
+    print("¡Conectado!")
+else:
+    print("No se puede conectar")
+
+
+
+#entramos a los directorios que necesitamos y creamos la carpeta de almacenamiento
+ls = []
+ftp.cwd('debian') 
+ftp.retrlines('LIST', ls.append)
+[line for line in ls if line.startswith('dr') ]
+ls
+[line for line in ls if line.startswith('l') ]
+ls
+
+#Establecemos las Terminaciones
+a = ".html"
+b = ".msg"
+c = ".txt"
+
+#Descargamos los Archivoos
+for i in ls:
+    if c in i:
+        ftp.retrbinary("RETR %s" %c, open(c, 'wb').write)
+for i in ls:
+    if a in i:
+        if os.path.isfile(a):
+            ftp.retrbinary("RETR %s" %a, open(a, 'wb').write)
+
+for i in ls:
+    if b in i:
+        if os.path.isfile(b):
+            ftp.retrbinary("RETR %s" %b, open(b, 'wb').write)
+ftp.close()
+
+    
